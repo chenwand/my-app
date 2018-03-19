6bc4d73a461c9f4cc12854fc1bc98b974647fc7f
Carlos Oliveira <chenwand@gmail.com>
2018-03-19 15:48:33 -0300

    tudo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import $ from 'jquery';
+import uuid from 'uuid';
 import './App.css';
+import Todos from './Components/Todos';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			projects: [],
+			todos:[]
+		}
+	}
+	getTodos(){
+		$.ajax({
+			url:'http://jsonplaceholder.typicode.com/todos',
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({todos: data}, function(){
+					console.log(this.state);
+				});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.log(err);
+			}
+		})
+	}
+
+	getProjects(){
+		this.setState({projects: [
+			{
+				id:uuid.v4(),
+				title:'A',
+				category:'1'
+			},
+			{
+				id:uuid.v4(),
+				title:'B',
+				category:'2'
+			},
+			{
+				id:uuid.v4(),
+				title:'C',
+				category:'3'
+			},
+		]});
+	}
+
+	componentWillMount(){
+			this.getProjects();
+			this.getTodos();
+	}
+
+	componentDidMount(){
+		this.getTodos();
+	}
+
+	handleAddProject(project){
+		let projects = this.state.projects;
+		projects.push(project);
+		this.setState({projects:projects});
+	}
+	handleDeleteProject(id){
+		let projects = this.state.projects;
+		let index = projects.findIndex(x => x.id === id);
+		projects.splice(index, 1);
+		this.setState({projects:projects});
+	}
+	render() {
+		return (
+			<div className="App">
+			<AddProject addProject={this.handleAddProject.bind(this)}/>
+			<Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+			<hr/>
+			<Todos todos={this.state.todos}/>
+			</div>
+		);
+	}
 }
 
-export default App;
+
---
+++ src/Components/Todos.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+export default class Todos extends Component {
+  render() {
+    let todoItems;
+    if(this.props.todos){
+        todoItems = this.props.todos.map(todo => {
+           // console.log(project);
+           return (
+               <TodoItem key={todo.title} todo={todo}/>
+           );
+        })
+    }
+    return (
+      <div className="Todos">
+            {todoItems}
+      </div>
+    );
+  }
+}
+
+ 
